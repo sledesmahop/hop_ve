6b3c28bb4556d59045eb6081b86fcd98436aa635
Sebastian Ledesma <sledesma@hopenvios.com.ar>
2026-02-19 18:26:56 -0300

    next build

--- _next/static/c0PMBrqIQ8EhTT6yF9y94/_buildManifest.js
+++ _next/static/c0PMBrqIQ8EhTT6yF9y94/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/hop_ve//_next/:path+",
+        "destination": "/hop_ve/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
